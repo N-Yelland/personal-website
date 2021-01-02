bc876b74555b439a0ae7fb6d9fe9404bf2ba8544
N-Yelland <34349938+N-Yelland@users.noreply.github.com>
2021-01-02 00:47:39 +0000

    developed hangman

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -18,7 +18,7 @@ fetch('big_wordlist.txt')
     // filter out all words that contain characters not present in alph
     // full_dict contains all words
     full_dict = raw_dict.filter(function(word){
-        return word.split('').every(char => alph.includes(char));
+        return word.split('').every(char => alph.includes(char));   
     });  
 });
 
@@ -32,18 +32,29 @@ function show(elementid) {
 function startNewGame() {
     // hides all objects
     Array.from(game.children).forEach(child => child.classList.add('hidden'));
+    hide('guesses');
     // unhide length-input
     show('length-input');
+    
+    //begins image loop, as global variable
+    imgloop = setInterval(nextImage, 500);
 }
 
 function submitLength() {
     var length = document.getElementById('length-input-box').value;
+    
+    // stops image loop
+    clearInterval(imgloop);
+    var imgdiv = document.getElementById('image');
+    imgdiv.src = 'images/0-fails.jpg';
+    imgdiv.setAttribute('data-num', 0);
     
     // hide length-input
     hide('length-input');
     
     // build blanks-row
     var table = document.getElementById('blanks');
+    table.innerHTML = '';
     var row = document.createElement('tr');
     table.appendChild(row);
     row.id = 'top-row'
@@ -55,11 +66,23 @@ function submitLength() {
         
     }
     show('blanks-row');
+    show('guesses');
     
     //global, list of words of correct length
     dict = full_dict.filter(word => word.length == length);    
     
-    startGuessing(length);
+    //global, letters of word, unknown letters are '_'
+    word = Array(length)
+    word .fill('_');
+    
+    //global, list of guessed characters
+    guesses = [];
+    
+    // dictionary of possible words
+    var d_pos = dict;
+    // (intialised to dict, updated after each guess)
+    
+    makeGuess(d_pos);
 }
 
 function getMatches(wordlist, pattern, guesses) {
@@ -82,31 +105,34 @@ function rankChar(char, wordlist) {
     return Math.abs(n/wordlist.length - 0.5);
 }
 
-function startGuessing(length) {
-    
-    //global, letters of word, unknown letters are '_'
-    word = Array(length)
-    word .fill('_');
-    
-    //global, list of guessed characters
-    guesses = [];
-    
-    // dictionary of possible words
-    var d_pos = dict;
-    // (intialised to dict, updated after each guess)
-    
-    makeGuess(d_pos);
-}
-
 function makeGuess(wordlist){
     
-    if (wordlist.length == 1) {
+    if (wordlist.length <= 1) {
         // ready to guess entire word
-        console.log('Guess entire word!');
-    } else if (wordlist.length == 0) {
-        // word not in dictionary
-        console.log('Your word isn\'t in my dictionary.');
+        
+        hide('guess-row');
+        show('final-guess');
+        var msg = document.getElementById('final-guess-msg');
+        
+        if (wordlist.length == 1) {
+            msg.innerHTML = 'I think your word is <span class="blue">'+ wordlist[0] + '</span>';
+            
+            var letters = Array.from(document.getElementById('top-row').children);
+            for (i=0; i < letters.length; i++) {
+                if (letters[i].innerHTML == '_') {
+                    letters[i].innerHTML = '<span class="blue">' + wordlist[0][i] + '</span>';
+                }
+            }
+            
+        } else {
+            msg.innerHTML = 'Your word isn\'t in my dictionary!'
+        }
     } else {
+        // console info
+        var N = wordlist.length;
+        console.log('Considering ' + N + ' words, expecting to need '
+                    + Math.round(Math.log2(N)) + ' guesses.');
+        
         // characters that can be guessed
         var charset = new Set(wordlist.join('').split(''));
         var pos_chars = Array.from(charset).filter(char => !guesses.includes(char));
@@ -119,8 +145,7 @@ function makeGuess(wordlist){
 
         guesses.push(guess);
 
-        //guess 'ends' here, needs to update html
-
+        // update html
         var letters = Array.from(document.getElementById('top-row').children);
         for (td of letters) {
             if (td.innerHTML == '_') {
@@ -134,6 +159,8 @@ function makeGuess(wordlist){
             }
         }
         
+        document.getElementById('guesses').innerHTML = guesses.join(' ');
+        
         show('guess-row');
         var msg = document.getElementById('guess-msg');
         msg.innerHTML = 'I guess... ' + guess;
@@ -144,25 +171,61 @@ function submitLetters() {
     
     // update html
     var letters = Array.from(document.getElementById('top-row').children);
+    var hit = false
     for (i=0; i < letters.length; i++) {
         if (letters[i].children.length > 0) {
             word[i] = letters[i].children[0].innerHTML;
+            if (!(word[i] == '_')) {
+                hit = true;
+            }
             letters[i].innerHTML = word[i];
         }
     }
     
-    var d_pos = getMatches(dict, word, guesses);
-    console.log(d_pos);
-    
-    makeGuess(d_pos);
+    // guessed wrong...
+    if (!hit) {
+        // out of lives?
+        if (nextImage() == 9) {
+            hide('guess-row');
+            show('final-guess');
+            var msg = document.getElementById('final-guess-msg');
+            msg.innerHTML = 'I\'m out of lives, you win!'
+                + '<br>Was your word any of these?<br>' + getMatches(dict, word, guesses).join(' ')
+            ;
+        
+        // carry on...
+        } else {
+            var d_pos = getMatches(dict, word, guesses);
+            makeGuess(d_pos);
+        }
+    } else {
+        // guessed last letter...
+        if (!word.includes('_')) {
+            hide('guess-row');
+            show('final-guess');
+            var msg = document.getElementById('final-guess-msg');
+            msg.innerHTML = 'I win! Your word is <span class="blue">' + word.join('') + '</span>';
+
+        // guessed correct but not finished...
+        } else {
+            var d_pos = getMatches(dict, word, guesses);
+            makeGuess(d_pos);
+        }
+    }
 }
 
 function clickLetter() {
     this.innerHTML = (this.innerHTML == '_') ? guess : '_';
 }
 
-$(document).ready(function(){    
+function nextImage() {
+    var imgdiv = document.getElementById('image');
+    var n = (parseInt(imgdiv.getAttribute('data-num')) + 1) % 10;
+    imgdiv.setAttribute('data-num', n);
+    imgdiv.src = 'images/' + n + '-fails.jpg';
+    return n;
+}
 
+$(document).ready(function(){    
     startNewGame();
-    
 });
