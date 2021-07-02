c93aeac9fb3ebf679d544e266243b856c63ef190
N-Yelland <34349938+N-Yelland@users.noreply.github.com>
2021-07-02 14:09:25 +0100

    Added 5th xword

--- xwords/xwords.js
+++ xwords/xwords.js
@@ -8,7 +8,7 @@ if (window.setting_tools) {
     $("h1").html("Crossword Setting Tool");
     $("#table-div").show();
     $("#puzzle-menu, #game-menu").hide();
-    buildGrid("xword_json/cxw004.json");
+    buildGrid("xword_json/cxw005.json");
 }    
 
 $("#puzzle-menu > div").on("click", function(){
